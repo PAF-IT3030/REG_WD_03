ff984a5adc42c5a32a540d67f61fc82f4a735001
Dilshan-686 <125535919+Dilshan-686@users.noreply.github.com>
2024-05-07 22:14:07 +0530

    login update

--- backend/src/main/java/com/Lifted/Lifted/Fitness/Application/config/RestTemplateConfig.java
+++ backend/src/main/java/com/Lifted/Lifted/Fitness/Application/config/RestTemplateConfig.java
@@ -3,6 +3,11 @@ package com.Lifted.Lifted.Fitness.Application.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,5 +15,17 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate webClientBuilder() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(List.of("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/", configuration);
+        return source;
     }
 }
